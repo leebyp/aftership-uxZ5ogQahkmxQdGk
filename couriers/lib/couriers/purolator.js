73b58f3550c8bedb9821d460bc00f1386f178f61
Benjamin Lee <leebyp@gmail.com>
2014-08-24 02:09:44 +0100

    Added dummy test to debug Purolator tracking

--- couriers/lib/couriers/purolator.js
+++ couriers/lib/couriers/purolator.js
@@ -63,6 +63,8 @@
 
 			console.log($.html());
 		}
+		
+		callback(null, trackings);
 	};
 
 	module.exports = Purolator;
--- test/test.js
+++ test/test.js
@@ -1,2 +1,25 @@
 // write your test case here
 
+var Couriers = require('../couriers/couriers');
+var Trackings = require('../couriers/lib/models/trackings');
+var Tracking = require('../couriers/lib/models/tracking');
+
+describe('Dummy test to track Purolator: ', function() {
+  // override the default 2000ms timeout
+  this.timeout(0);
+
+  it('#330218400340', function(done) {
+    var courier = Couriers.getCourier('Purolator');
+    var trackings = new Trackings();
+    trackings.push(new Tracking({
+      tracking_number: '330218400340'
+    }));
+
+    courier.getTrackings(trackings, function(err, trackings) {
+      console.log('calling getTrackings final callback...');
+      done();
+    });
+    
+  });
+
+});
