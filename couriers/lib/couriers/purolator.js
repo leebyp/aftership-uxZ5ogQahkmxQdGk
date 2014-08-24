f66097b508dc26edae5bdf6f9988b13741328121
Benjamin Lee <leebyp@gmail.com>
2014-08-25 00:40:53 +0100

    Parse method updates origin, destination addresses and shipment info

--- couriers/lib/couriers/purolator.js
+++ couriers/lib/couriers/purolator.js
@@ -13,6 +13,7 @@
 	var GeneralError = require('../models/general_error');
 
 	var Cheerio = require('cheerio');
+	var moment = require('moment');
 
 	// config
 	var config = {
@@ -56,14 +57,85 @@
 	Purolator.prototype.parse = function(trackings, results, callback) {
 		console.log(Utility.Timer.micro() + ': ' + this.config.slug + ' parse');
 
+		function _updateAddress(original, update_info) {
+			if (update_info.country) {
+				original.setCountryIso3(update_info.country);
+			}
+			if (update_info.province) {
+				original.setState(update_info.province);
+			}
+			if (update_info.city) {
+				original.setCity(update_info.city);
+			}
+			if (update_info.postalcode) {
+				original.setZip(update_info.postalcode);
+			}
+			if (update_info.address) {
+				original.setStreetAddress(update_info.address);
+			}
+			return original;
+		}
+
+		function _updateShipment(original, update_info) {
+			if (update_info.product) {
+				original.setType(update_info.product);
+			}
+			if (update_info.weight.estimated) {
+				original.setWeight(update_info.weight.estimated);
+			}
+			if (update_info.weight.unit) {
+				original.setWeightUnit(update_info.weight.unit.toLowerCase());
+			}
+			if (update_info.actualDelivery) {
+				original.setDeliveredDate(moment.utc(update_info.actualDelivery, 'YYYY-MM-DDTHH:mm:ss'));
+			}
+			if (update_info.estimatedDelivery.deliverBy) {
+				original.setScheduledDeliveryDateString(update_info.estimatedDelivery.deliverBy, 'YYYY-MM-DD HH:mm');
+			}
+			if (update_info.deliveryContact) {
+				original.setSignedBy(update_info.deliveryContact);
+			}
+			return original;
+		}
+
 		for (var i=0; i<trackings.length; i++) {
+			var tracking = trackings[i];
+
 			var checkpoints = [];
-			var data = results[trackings[i].getTrackingNumber()];
+			var data = results[tracking.getTrackingNumber()];
 			var $ = Cheerio.load(data);
 
-			console.log($.html());
+			var useful = $('#1359654114029').text();
+			useful = useful.substr(0, useful.length-205);
+
+			// @NOTE: potentially dangerous eval statement
+			var history, details, associated;
+			eval(useful +
+				'\n history = jsHistoryTable' +
+				'\n details = detailsData' +
+				'\n associated = assocPackages');
+
+			// update origin and destination addresses of tracking
+			var updated_origin = _updateAddress(tracking.getOriginAddress(), details.origin);
+			tracking.setOriginAddress(updated_origin);
+			var updated_destination = _updateAddress(tracking.getDestinationAddress(), details.destination);
+			tracking.setDestinationAddress(updated_destination);
+
+			// update shipment info
+			var updated_shipment = _updateShipment(tracking.getShipment(), details);
+			tracking.setShipment(updated_shipment);
+
+			// console.log('=================');
+			// console.log(tracking);
+			// console.log('=================');
+			// console.log(history);
+			// console.log('=================');
+			// console.log(details);
+			// console.log('=================');
+			// console.log(associated);
+			// console.log('=================');
 		}
-		
+
 		callback(null, trackings);
 	};
 
